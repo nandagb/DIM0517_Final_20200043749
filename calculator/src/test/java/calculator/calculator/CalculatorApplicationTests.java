0d8cb2ac55c2676a3a362f8de7423ccc8be0134e
nandagb <nandagb23@gmail.com>
2024-07-31 14:29:09 -0300

    c3

--- calculator/src/test/java/calculator/calculator/CalculatorApplicationTests.java
+++ calculator/src/test/java/calculator/calculator/CalculatorApplicationTests.java
@@ -1,15 +1,35 @@
 package calculator.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
+import static sun.nio.cs.Surrogate.is;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class CalculatorApplicationTests {
-
+	@Autowired
+	Calc calculadora;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testAdd(){
+		assertTrue(calculadora.add(3, 4) == 7);
+	}
+
+	void testSub1(){
+		assertTrue(calculadora.sub(4, 3) == 1);
+	}
+
+	void testSub2(){
+		assertTrue(calculadora.sub(3, 4) == -1);
 	}
 
 }
